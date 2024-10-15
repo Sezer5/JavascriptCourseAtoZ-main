049fd812162c88776bb8b80b09f721f6814e05af
Sezer Ünalmış <unalmissezer@gmail.com>
2024-10-15 11:45:21 +0300

    Until the global and local varible videos

---
+++ Lesson18 Switch Statement/main.js
@@ -0,0 +1,26 @@
+const not = 'FF';
+
+switch(not){
+
+    case 'AA':
+        console.log('En yüksek not');
+        break;
+    case 'BA':
+        console.log('Yüksek not');
+        break;
+    case 'BB':
+        console.log('İyi not');
+        break;
+    case 'CB':
+        console.log('İdare eder not');
+        break;
+    case 'CC':
+        console.log('Geçer Not');
+        break;
+    default:
+        console.log('kaldınız');
+        break;
+
+
+
+}
---
+++ Lesson19 Global And Local Variables/main.js
@@ -0,0 +1,15 @@
+let yas = 29;
+
+if(true){
+    let yas=36;
+    console.log("İçerideki:",yas);
+    if(true){
+        let yas =50;
+        var can =22;
+        let test=32;
+        console.log("İçeridekinin içindeki:",yas,can,test)
+        console.log(can);
+    }
+}
+
+console.log("Dışarıdaki:",yas,can,test); 
