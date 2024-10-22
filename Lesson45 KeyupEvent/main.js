6bc946eb578118542f8350cda98cdfcd3e0ffccb
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2024-10-22 17:37:09 +0300

    Chapter 7 Ended

--- Lesson45 KeyupEvent/main.js
+++ Lesson45 KeyupEvent/main.js
@@ -1,15 +1,28 @@
 const form = document.querySelector('.signupForm');
 const message = document.querySelector('.message');
-
+const usernamePattern = /^[a-z]{6,10}$/;
 
 form.addEventListener('submit', e=>{
     e.preventDefault();
     const username = form.username.value;
-    const usernamePattern = /^[a-z]{6,10}$/;
+    
 
     if(usernamePattern.test(username)){
         message.textContent='Başarılı';
     }else{
         message.textContent = 'Lütfen Hepsi Küçük Harf ve 6 ile 10 Karakter arası seçim yapınız';
     }
+})
+
+form.username.addEventListener('keyup',e=>{
+    // console.log(e.target.value);
+
+    if(usernamePattern.test(e.target.value)){
+        
+        form.username.setAttribute('class','success');
+
+    }else{
+        form.username.setAttribute('class','error');
+    }
+
 })
