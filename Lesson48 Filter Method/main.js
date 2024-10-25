26eab567ba87f7132a962fdd76ee02d7cc95c021
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2024-10-25 17:57:31 +0300

    Last

--- Lesson48 Filter Method/main.js
+++ Lesson48 Filter Method/main.js
@@ -1,25 +1,30 @@
-const correctAnswers = ['E','E','E','E'];
+const correctAnswers = ['8','10','3','25'];
 
 const form = document.querySelector('.question-form');
 
 const result = document.querySelector('.result');
+const successMessage = document.querySelector('#successMessage');
+const btn = document.querySelector('.btn');
 
 form.addEventListener('submit',e=>{
     e.preventDefault();
     let score = 0;
-    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q3.value];
+    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value];
     userAnswers.forEach((answer,index)=>{
         if(answer === correctAnswers[index]){
             score += 25;
         }
     });
-
+    if(score == 100){
+        successMessage.classList.remove('d-none');
+        btn.setAttribute('disabled','');
+    }
     // console.log(score);
 
     result.classList.remove('d-none');
     let puan = 0;
     const bastir = setInterval(()=>{
-        result.querySelector('span').textContent = `${puan}%`;
+        result.querySelector('#percentage').textContent = `${puan}%`;
         if(puan==score){
             clearInterval(bastir);
         }else{
