94eb788dab696efaf0c04d7c41b8332cf8fbd9da
Sezer Ünalmış <unalmissezer@gmail.com>
2024-12-15 16:06:56 +0300

    Delete Data

---
+++ 14 Javascript LocalStorage/74 Delete Data/index.js
@@ -0,0 +1,21 @@
+localStorage.setItem('name','Sezer'); // LOCAL STORAGE'DE ITEMLER STRING OLARAK TUTULMAKTADIR.
+localStorage.setItem('yas','34');
+
+let ad = localStorage.getItem('name');
+let age = localStorage.getItem('yas');
+console.log(ad,age);
+
+localStorage.setItem('name','Sezer Ünalmış');
+localStorage.yas = '35';
+
+ad = localStorage.getItem('name');
+age = localStorage.getItem('yas');
+console.log(ad,age);
+
+// localStorage.removeItem('name'); TEKLİ SİLME İŞLEMİ
+
+localStorage.clear(); // ÇOKLU SİLME İŞLEMİ
+
+ad = localStorage.getItem('name');
+age = localStorage.getItem('yas');
+console.log(ad,age);
