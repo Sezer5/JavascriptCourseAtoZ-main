41181c121a1d629b16749d85b3eae69c2ae1a753
Sezer Ünalmış <unalmissezer@gmail.com>
2024-12-21 23:31:20 +0300

    start

--- 20 Foreign Currency Converter Project/91 Complete/index.js
+++ 20 Foreign Currency Converter Project/91 Complete/index.js
@@ -1,2 +1,15 @@
-Your API Key: 6dafece7fa46538dfc6d5558
-Example Request: https://v6.exchangerate-api.com/v6/6dafece7fa46538dfc6d5558/latest/USD
+// Your API Key: 6dafece7fa46538dfc6d5558
+// Example Request: https://v6.exchangerate-api.com/v6/6dafece7fa46538dfc6d5558/latest/USD
+
+const currencyFirst = document.querySelector('#currencyFirst');
+const currencySecond = document.querySelector('#currencySecond');
+const count = document.querySelector('#count');
+const equal = document.querySelector('#equal');
+const exchangeRate = document.querySelector('#exchangeRate');
+
+updateRate();
+
+function updateRate(){
+    fetch()
+}
+
