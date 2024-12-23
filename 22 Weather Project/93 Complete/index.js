7a8f9666e08da29844614d25a45431741fc1b87f
Sezer Ünalmış <unalmissezer@gmail.com>
2024-12-23 22:38:26 +0300

    Weather Heat Project Finished

--- 22 Weather Project/93 Complete/index.js
+++ 22 Weather Project/93 Complete/index.js
@@ -1,10 +1,26 @@
-const accordions = document.querySelectorAll('.accordion');
+const celcius = document.getElementById('celcius');
+const fahrenheit = document.getElementById('fahrenheit');
+const kelvin = document.getElementById('kelvin');
 
-accordions.forEach(accordion =>{
-    const accordionButton = accordion.querySelector('.accordionButton');
-    accordionButton.addEventListener('click',()=>{
-        accordion.querySelector('.minusIcon').classList.toggle('active');
-        accordion.querySelector('.plusIcon').classList.toggle('active');
-        accordion.querySelector('.accordionText').classList.toggle('active');
-    })
-})
+function calculateTemp(event){
+    
+    const currentValue = Number(event.target.value);
+
+    switch(event.target.name){
+
+        case 'celcius':
+            kelvin.value = (currentValue+273.32).toFixed(2);
+            fahrenheit.value = (currentValue*1.8+32).toFixed(2);
+            break;
+        case 'fahrenheit':
+            kelvin.value = ((currentValue-32)/1.8+273.32).toFixed(2);
+            celcius.value = ((currentValue-32)/1.8).toFixed(2);
+            break;
+        case 'kelvin':
+            celcius.value = (currentValue-273.32).toFixed(2);
+            fahrenheit.value = (((currentValue-273.32)*1.8)+32).toFixed(2);
+            break;
+        default:
+            break;
+    }
+}
